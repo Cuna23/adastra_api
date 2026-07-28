83d0879aeb5d5eb54c9e5b1b7e48642c7b531225
NUR ANIS SYAKIRAH BINTI AWANG ZAKARIA <nuranis1910@gmail.com>
2026-07-28 11:38:02 +0800

    fixed gmbr company

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/storage/{path}', function (string $path) {
+    \Log::info('STORAGE ROUTE HIT', ['path' => $path]); 
+    
+    $fullPath = storage_path('app/public/' . $path);
+    $headers = ['Access-Control-Allow-Origin' => '*'];
+
+    if (!file_exists($fullPath)) {
+        return response('File not found', 404, $headers);
+    }
+    return response()->file($fullPath, $headers);
+})->where('path', '.*');
